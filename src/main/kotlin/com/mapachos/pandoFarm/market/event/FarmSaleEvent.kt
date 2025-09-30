2df48a135eaf77312ee47fd250fb032db94f7895
AlepandoLaptop <aocamporam@gmail.com>
2025-09-30 16:18:34 -0600

    Remove redundant class body from FarmSaleEvent declaration

--- src/main/kotlin/com/mapachos/pandoFarm/market/event/FarmSaleEvent.kt
+++ src/main/kotlin/com/mapachos/pandoFarm/market/event/FarmSaleEvent.kt
@@ -3,5 +3,4 @@ package com.mapachos.pandoFarm.market.event
 import com.mapachos.pandoFarm.plants.engine.event.PlayerFarmEvent
 import org.bukkit.entity.Player
 
-class FarmSaleEvent(player: Player, ): PlayerFarmEvent(player) {
-}
+class FarmSaleEvent(player: Player, ): PlayerFarmEvent(player)
